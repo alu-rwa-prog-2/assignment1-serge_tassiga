69f44fc2f3fded7869a7c919f10c2c0826f368d6
ALUtech166 <70975693+ALUtech166@users.noreply.github.com>
2021-01-23 22:15:41 +0200

    Add files via upload

---
+++ Question1.java
@@ -0,0 +1,20 @@
+// Author = Serge Tassiga
+// First Project in Java 
+public class Question1 {
+    public static void main(String[] args) {
+        // this is the name I gave to my person
+        System.out.println("I'm Serge");
+        // This an example of my height and weight
+        double my_height = 10;
+        double my_weight = 50;
+        // The Final result which the BMI of my person
+        System.out.println("my BMI is = " + (my_weight/(my_height*2)));
+        // example of two integers
+        int length = 5;
+        int width = 10;
+        // this is the perimenter
+        System.out.println("The perimeter of the Triangle = " + ((length + width)/2));
+        // this is the perimenter
+        System.out.println("The surface of the Triangle = " + (length*width));
+    }
+}
